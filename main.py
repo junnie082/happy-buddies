1d250c0c8c3a8f8e1f787ce76afcd16fcf7b6065
Hyojeong Jun <junnie082@gmail.com>
2023-11-14 02:42:27 +0900

    fix: main.py is fixed as it should be

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
-# 멤버이름 : [활동학기수, 가능날짜]
-members = {
-    "류창수": [10, []], "진성우": [10, []], "유도현": [10, []], "한종한": [10, []],
-    "한영숙": [10, []], "김해슬": [10, []],"김우찬": [10, []], "박찬경": [10, []],
-    "김선국": [8, []], "최태오": [8, []], "김성민": [8, []], "함민우": [8, []],
-    "이용헌": [8, []], "이수연": [8, []], "서연우": [8, []], "안민혁": [8, []],
-    "강지수": [6, []], "전효정": [6, []], "최승혜": [6, []], "박수겸": [6, []],
-    "오효석": [6, []], "이의린": [4, []], "임채영": [4, []], "윤희정": [4, []],
-    "김담우": [4, []], "김찬희": [4, []], "장승호": [4, []], "방재현": [4, []],
-    "정기호": [4, []], "조시은": [2, []], "성예진": [2, []], "심혜원": [2, []],
-    "이준": [2, []], "문성원": [2, []], "김형준": [2, []]
-}
+from members import Members
+from dates import Dates
+from counting_table import CountingTable
 
+mem = Members()
+dates = Dates()
 months = list(map(int, input().split()))
-calculate_days = lambda x: 30 + (x%2 if  x <= 7  else  (0 if x % 2 else 1))
-first_days = calculate_days(months[0])
-second_days = calculate_days(months[1])
-firstMonth = [[] for _ in range(first_days)]
-secondMonth = [[] for _ in range(second_days)]
+
+dates.set_month(months)
+dates.days_in_first_month()
+dates.days_in_second_month()
+
+cnt_table = CountingTable(months)
 
 while(1):
     name = input()
@@ -27,27 +21,32 @@ while(1):
         date = input()
         if date == "날짜끝":
             break
+        month, day = map(int, date.split('.'))
 
-        members[name][1].append(date)
+        mem.addPossibleDateInMember(name, date)
 
-        if int(date.split('.')[0]) == months[0]:
-            firstMonth[int(date.split('.')[1])].append(name)
+        if month == dates.months[0]:
+            dates.firstMonth[day-1].append(name)
+            if mem.members[name][0] <= 2:
+                cnt_table.firstMonth[day-1][0] += 1
+            else:
+                cnt_table.firstMonth[day-1][1] += 1
         else:
-            secondMonth[int(date.split('.')[1])].append(name)
+            dates.secondMonth[day-1].append(name)
+            if mem.members[name][0] <= 2:
+                cnt_table.secondMonth[day-1][0] += 1
+            else:
+                cnt_table.secondMonth[day-1][1] += 1
 
-        members[name][1].append(date)
-
-firstMonth.sort(reverse=True)
-secondMonth.sort(reverse=True)
+        mem.members[name][1].append(date)
 
 
-
-for i in firstMonth:
+for i in cnt_table.firstMonth:
     print(i, end=" ")
 
 print()
 
-for i in secondMonth:
+for i in cnt_table.secondMonth:
     print(i, end=" ")
 
 # for name, values in members.items():
